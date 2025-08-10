4bee893955b41d1708f249890c8c24f1c6a7553e
Luis Goncalves <154645283+LLuisGL@users.noreply.github.com>
2025-08-10 12:23:19 -0500

    fix: Bug with images per frame (Image flickering with an imagen that did not correspond to the moment)

--- Assets/Scripts/Scenes/cart/BackgroundAnimator.cs
+++ Assets/Scripts/Scenes/cart/BackgroundAnimator.cs
@@ -1,70 +1,85 @@
-using UnityEngine;
+using System;
 using System.Collections;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Versioning;
+using UnityEngine;
 
 public class BackgroundAnimator : MonoBehaviour
 {
     private Sprite[] currentSprites;
     private int currentIndex = 0;
     private SpriteRenderer spriteRenderer;
+    private bool isSpriteRunning = false;
 
-    public float spriteChangeInterval = 0.2f;
+    public float spriteChangeInterval = .5f;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
 
-        Sprite[] straightSprites = Resources.LoadAll<Sprite>("Backgrounds/Surf/Straight");
-        Sprite[] leftTurnSprites = Resources.LoadAll<Sprite>("Backgrounds/Surf/Left");
-        Sprite[] rightTurnSprites = Resources.LoadAll<Sprite>("Backgrounds/Surf/right");
-        currentSprites = straightSprites;
+        Sprite[] SSPrites = Resources.LoadAll<Sprite>("Backgrounds/Surf/S");
+        Sprite[] LRSprites = Resources.LoadAll<Sprite>("Backgrounds/Surf/LR");
+        currentSprites = SSPrites.OrderBy(s => int.Parse(System.Text.RegularExpressions.Regex.Match(s.name, @"\d+").Value)).ToArray();
 
         StartCoroutine(AnimateBackground());
-        StartCoroutine(SimulateDirectionChange(straightSprites, leftTurnSprites, rightTurnSprites));
+        StartCoroutine(SimulateDirectionChange(SSPrites, LRSprites)); 
     }
 
     IEnumerator AnimateBackground()
     {
         while (true)
         {
-            if (currentSprites.Length > 0)
-            {
-                spriteRenderer.sprite = currentSprites[currentIndex];
-                currentIndex = (currentIndex + 1) % currentSprites.Length;
+            UnityEngine.Debug.Log("Esta en ! " + currentSprites.Length);
+            foreach(Sprite sprite in currentSprites) {
+
+                spriteRenderer.sprite = sprite;
+                UnityEngine.Debug.Log("Cambio el Sprite! " + sprite);
+                yield return new WaitForSeconds(0.075f);
+
             }
-            yield return new WaitForSeconds(spriteChangeInterval);
+            isSpriteRunning = false;
         }
+
+        yield return new WaitForSeconds(.5f);
     }
 
-    IEnumerator SimulateDirectionChange(Sprite[] straight, Sprite[] left, Sprite[] right)
+    IEnumerator SimulateDirectionChange(Sprite[] straight, Sprite[] lr)
     {
         while (true)
         {
-            yield return new WaitForSeconds(8f);
-
-            int rand = Random.Range(0, 3);
-            switch (rand)
+            yield return new WaitForSeconds(0.005f);
+            if (!isSpriteRunning)
             {
-                case 0:
-                    SetSprites(straight);
-                    Debug.Log("Cambiado a Recto");
-                    break;
-                case 1:
-                    SetSprites(left);
-                    Debug.Log("Cambiado a Izquierda");
-                    break;
-                case 2:
-                    SetSprites(right);
-                    Debug.Log("Cambiado a Derecha");
-                    break;
+                isSpriteRunning = true;
+                int rand = UnityEngine.Random.Range(0, 4);
+                UnityEngine.Debug.Log(rand);
+                switch (rand)
+                {
+
+                    case 0:
+                        SetSprites(straight, false);
+                        break;
+                    case 1:
+                        SetSprites(straight, false);
+                        break;
+                    case 2:
+                        SetSprites(lr, true);
+                        break;
+                    case 3:
+                        SetSprites(lr, false);
+                        break;
+                }
             }
-        }
+
+        }    
     }
 
-    void SetSprites(Sprite[] newSprites)
+    void SetSprites(Sprite[] newSprites, bool flipX)
     {
         if (newSprites == null || newSprites.Length == 0) return;
-        currentSprites = newSprites;
-        currentIndex = 0;
-        spriteRenderer.sprite = currentSprites[currentIndex];
+        currentSprites = newSprites.OrderBy(s => int.Parse(System.Text.RegularExpressions.Regex.Match(s.name, @"\d+").Value)).ToArray();
+        spriteRenderer.sprite = currentSprites[0];
+        spriteRenderer.flipX = flipX;
     }
 }
